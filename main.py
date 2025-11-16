e0ed0710e99f6a6b1d3ee24633c7a801a27e0753
alisher876 <a.kurashev@newuu.uz>
2025-11-16 23:17:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,8 @@ def home():
     return "Bot is running!"
 
 def run_flask():
-    flask_app.run(host="0.0.0.0", port=3000)
+    port = int(os.environ.get("PORT", 5000))  # default 5000 if not set
+    flask_app.run(host="0.0.0.0", port=port)
 
 Thread(target=run_flask).start()
 app_bot.run_polling()
